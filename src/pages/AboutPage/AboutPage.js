34cf13a8bbcb0fc64f58385891adc7af727aa7dd
Gangwani, Pawan <pgangwan@jcp.com>
2019-01-15 02:34:16 +0530

    verbose

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -33,7 +33,7 @@ const AboutPage = (props, context) => {
               I stay close to the community and try to keep tabs with the pace at which the web is evolving. I also like to <span className="highlight">demo</span> what I learn.
             </p>
             <p>
-            React-Redux, Node.js, HTML, CSS, CI/CD and <span className="highlight">devtools</span> are the main tricks up my sleeve. I am also obsessed with <span className="highlight">making the code 100% tested</span> using testing tools viz Mocha/Enzyme for React.js codebase. I believe that <span className="highlight">Agile methodology</span>, not only, helps project management but also develops a mentality of Continuous Delivery in devs.
+            React-Redux, Node.js, HTML, CSS, CI/CD and <span className="highlight">devtools</span> are the main tricks up my sleeve. I am also obsessed with thinking of <span className="highlight">making the code 100% tested</span> using testing tools viz Mocha/Enzyme for React.js codebase. I believe that <span className="highlight">Agile methodology</span>, not only, helps project management but also develops a mentality of Continuous Delivery in devs.
             </p>
             <p>
               In my free time, I enjoy playing <span className="highlight">Table Tennis</span> <i  className="fas fa-table-tennis"></i> , listening to music <i class="fas fa-headphones"></i>, watching Bollywood movies <i class="fas fa-film"></i> and spending time with my family <i class="fas fa-heartbeat"></i> . Moreover, I am a <span className="highlight">foodie</span> and hence, I love exploring new restros  <i class="fas fa-utensils"></i> and cuisines whenever I get time.
