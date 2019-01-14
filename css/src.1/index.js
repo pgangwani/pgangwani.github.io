861fb277bae6fee66d13339552bddc030e48894f
Gangwani, Pawan <pgangwan@jcp.com>
2019-01-14 22:19:33 +0530

    removed duplicate files

--- css/src.1/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './libs/fontawesome/fontawesome-all.min';
-import './styles/base.scss';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
