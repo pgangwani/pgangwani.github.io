03a7d65e9f66cc3551e1676f9e9c6587476490f5
Gangwani, Pawan <pgangwan@jcp.com>
2019-02-10 18:50:06 +0530

    Updates

---
+++ precache-manifest.7176ee520903aeaca517ac5f7cc67dba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e17a64836c0412759543f90460d0ed1",
+    "url": "/static/media/roboto-900.2e17a648.woff2"
+  },
+  {
+    "revision": "ece6673e477b4d7aca12f04dace5ed60",
+    "url": "/static/media/roboto-400.ece6673e.woff2"
+  },
+  {
+    "revision": "c0cc9c92fb877993ea6d421c31d33e09",
+    "url": "/static/media/roboto-300.c0cc9c92.woff2"
+  },
+  {
+    "revision": "9a8a1f8c8f9860224f449c21a3bd76d4",
+    "url": "/static/media/roboto-100.9a8a1f8c.woff2"
+  },
+  {
+    "revision": "d8f8811d59f2a4935f145012efb7e98f",
+    "url": "/static/media/reem-kufi-v4-latin-regular.d8f8811d.woff2"
+  },
+  {
+    "revision": "20e4fd7eed6831c1778aa056a32b3244",
+    "url": "/static/media/reem-kufi-v4-latin-regular.20e4fd7e.woff"
+  },
+  {
+    "revision": "114a4a8166aaff1ec161",
+    "url": "/static/js/main.114a4a81.js"
+  },
+  {
+    "revision": "114a4a8166aaff1ec161",
+    "url": "/static/css/main.d73df0fa.css"
+  },
+  {
+    "revision": "945139b8f46ac2e35c16636761cb99a6",
+    "url": "/index.html"
+  }
+];
